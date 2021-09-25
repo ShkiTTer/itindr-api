12f024d4bdd8219962c6fba20374e4ed2cd6fdd7
Maxim Sachuk <msachuk@kode-t.ru>
2021-09-25 13:04:10 +0700

    Update migrations

--- src/main/resources/db/migration/V2__add_profile_and_topic.sql
+++ src/main/resources/db/migration/V2__add_profile_and_topic.sql
@@ -2,20 +2,20 @@ create table profile
 (
     id           uuid primary key,
     user_name    varchar(100) not null,
-    about_myself varchar(250),
-    avatar       varchar(255),
+    about_myself varchar(250) default null,
+    avatar       varchar(255) default null,
     user_id      uuid references users (id) on delete cascade
 );
 
 create table topic
 (
     id          uuid primary key,
-    topic_title varchar(255)  not null
+    topic_title varchar(255) not null
 );
 
 create table profile_topic
 (
     profile_id uuid references profile (id) on delete cascade,
-    topic_id uuid references topic (id) on delete cascade,
+    topic_id   uuid references topic (id) on delete cascade,
     constraint pk_profile_topic primary key (profile_id, topic_id)
 );
