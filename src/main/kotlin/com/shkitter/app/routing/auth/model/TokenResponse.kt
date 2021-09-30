216d8e6b07c4de81b689ee96799a6cb091c9d563
Maxim Sachuk <msachuk@kode-t.ru>
2021-09-30 23:27:17 +0700

    Fix response dates

--- src/main/kotlin/com/shkitter/app/routing/auth/model/TokenResponse.kt
+++ src/main/kotlin/com/shkitter/app/routing/auth/model/TokenResponse.kt
@@ -1,22 +1,26 @@
 package com.shkitter.app.routing.auth.model
 
+import com.shkitter.domain.common.utils.DateTimeUtils
 import com.shkitter.domain.token.TokenInfo
 import kotlinx.serialization.Serializable
+import java.time.format.DateTimeFormatter
 
 @Serializable
 data class TokenResponse(
     val accessToken: String,
-    val accessTokenExpiredAt: Long,
+    val accessTokenExpiredAt: String,
     val refreshToken: String,
-    val refreshTokenExpiredAt: Long
+    val refreshTokenExpiredAt: String
 ) {
 
     companion object {
         fun fromDomain(data: TokenInfo) = TokenResponse(
             accessToken = data.accessToken,
-            accessTokenExpiredAt = data.expiredAccess,
+            accessTokenExpiredAt = DateTimeUtils.fromSeconds(data.expiredAccess)
+                .format(DateTimeFormatter.ISO_ZONED_DATE_TIME),
             refreshToken = data.refreshToken,
-            refreshTokenExpiredAt = data.expiredRefresh
+            refreshTokenExpiredAt = DateTimeUtils.fromSeconds(data.expiredRefresh)
+                .format(DateTimeFormatter.ISO_ZONED_DATE_TIME)
         )
     }
 }
